103194af984b5e49ec7d32c6d091557ec039380b
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 18:03:08 +0300

    create frist interface in I UOW

--- ECommerce.Application/UnitOfWork/IUnitOfWork.cs
+++ ECommerce.Application/UnitOfWork/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerce.Application.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace ECommerce.Application.UnitOfWork
 {
     public interface IUnitOfWork
     {
+        IUserRepository UserRepository { get; }
     }
 }
