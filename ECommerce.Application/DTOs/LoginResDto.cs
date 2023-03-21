9439aed9bb36635ad7c9e38f5afa2296cec11f1b
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 20:06:31 +0300

    create login with Token

---
+++ ECommerce.Application/DTOs/LoginResDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class LoginResDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
