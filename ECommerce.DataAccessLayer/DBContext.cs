5418d1fdada64aa9d41fe42c9ed7a814469e5b63
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 17:24:57 +0300

    Create DB context within DAL

---
+++ ECommerce.DataAccessLayer/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccessLayer
+{
+    public class DBContext :DbContext
+    {
+        public DBContext(DbContextOptions option):base(option)
+        {
+            
+        }
+        public DbSet<User>Users { get; set; }
+    }
+
+}
