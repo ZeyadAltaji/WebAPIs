a53e8f6d5e404a99f3e883f03543ba46ca1dc18e
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 17:37:09 +0300

    create user Repo

---
+++ ECommerce.DataAccessLayer/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccessLayer.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DBContext dc;
+        public UserRepository(DBContext dc)
+        {
+            this.dc = dc;     
+        }
+        public async Task<User> Authenticate(string Email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(string Email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UserAlreadyExists(string Email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
