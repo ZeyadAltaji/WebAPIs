d6a24cd4862c5790493743a7f78b0491cdd2b868
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 18:07:05 +0300

    imp interface into class uow

---
+++ ECommerce.DataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Application.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccessLayer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DBContext dc;
+        public UnitOfWork(DBContext dc)
+        {
+            this.dc = dc;     
+        }
+        public IUserRepository UserRepository => throw new NotImplementedException();
+    }
+}
