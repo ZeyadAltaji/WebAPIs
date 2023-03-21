856ca0c2e8dd2035e117004c3478a1149941d218
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 17:32:22 +0300

    create abstraction Repo for user only

---
+++ ECommerce.Application/Abstractions/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Abstractions
+{
+    public interface IUserRepository
+    {
+        Task<User> Authenticate(string Email, string password);
+        void Register(string Email, string password);
+
+        Task<bool> UserAlreadyExists(string Email);
+    }
+}
