5758c3bea96a4c3ff602f8b67461eee50a6e37ad
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-21 17:13:40 +0300

    build frist models

---
+++ ECommerce.Domain/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Models
+{
+    public  class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PasswordKey { get; set; } = string.Empty;
+    }
+}
