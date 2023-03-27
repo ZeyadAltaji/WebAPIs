77b6a1d22faf2776287e1c306e0c286056e88d77
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-28 00:24:52 +0300

    create Frest TB for user

--- ECommerce.Domain/Models/User.cs
+++ ECommerce.Domain/Models/User.cs
@@ -9,9 +9,16 @@ namespace ECommerce.Domain.Models
     public  class User
     {
         public int ID { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Frist_Name { get; set; } = string.Empty;
+        public string Last_Name { get; set; }=string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string ComfirmPassword { get; set; } = string.Empty;
         public string PasswordKey { get; set; } = string.Empty;
+        public string Phone1 { get; set; } = string.Empty;
+        public string Phone2 { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int Role { get; set; }
+        public string Image { get; set; }=string.Empty;
     }
 }
