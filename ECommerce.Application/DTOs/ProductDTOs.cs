3683429038622906a2eb710cf59874a01eab9371
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:24:13 +0300

    create class produts Dtos with code

---
+++ ECommerce.Application/DTOs/ProductDTOs.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class ProductDTOs:BaseClass
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string main_Image { get; set; } = string.Empty;
+        public string sub_Image1 { get; set; } = string.Empty;
+        public string sub_Image2 { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public double offers { get; set; }
+        public double New_price { get; set; }
+        public int Quantity { get; set; }
+        public string Link { get; set; } = string.Empty;
+        public string Button { get; set; } = string.Empty;
+        public int Id_Brands { get; set; }
+        public Brands? Brands { get; set; }
+        public int Id_Car { get; set; }
+        public Car? Car { get; set; }
+        public int Category_Id { get; set; }
+        public Category? Category { get; set; }
+        public int Customer_Id { get; set; }
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+    }
+}
