4628bed614c3144488ef684944613a9cd8987790
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:29:18 +0300

    create class Category Dtos with code

---
+++ ECommerce.Application/DTOs/CategoryDTOs.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class CategoryDTOs :BaseClass
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+    }
+}
