2390f400baad133468b2cff20c700cba42c48049
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:32:02 +0300

    add validation into class user

--- ECommerce.Domain/Models/User.cs
+++ ECommerce.Domain/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,29 @@ namespace ECommerce.Domain.Models
     public  class User
     {
         public int Id { get; set; }
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
         public string Frist_Name { get; set; } = string.Empty;
+        [Required]
         public string Last_Name { get; set; }=string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
+        [Required]
         public string ComfirmPassword { get; set; } = string.Empty;
+        
         public string PasswordKey { get; set; } = string.Empty;
+        [Required]
         public string Phone1 { get; set; } = string.Empty;
+        [Required]
         public string Phone2 { get; set; } = string.Empty;
+        [Required]
         public string Address { get; set; } = string.Empty;
+
         public int Role { get; set; }
+        [Required]
         public string Image { get; set; } = string.Empty;
 
     }
