85134952184d6f9f8dce64e002c69b121ce72077
Zaid <Zaid@DESKTOP-G9L6CKN>
2023-03-30 23:46:41 +0300

    add baseinterface

---
+++ ECommerce.Application/Abstractions/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Abstractions
+{
+    public interface IRepository<TEntity>
+    {
+        public void Create(TEntity entity);
+        public void Update(int Id, TEntity entity);
+        public void Delete(int Id, TEntity entity);
+
+        TEntity GetByID(int id);
+
+        List<TEntity> GetAll();
+    }
+}
