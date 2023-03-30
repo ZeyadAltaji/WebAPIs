6bacd2d485a09db090cd84948ca8441e4d3c43a2
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:29:32 +0300

    create class Cars Dtos with code

---
+++ ECommerce.Application/DTOs/CarDTOs.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class CarDTOs : BaseClass
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Class { get; set; } = string.Empty;
+        public DateTime Production_Date { get; set; }
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+    }
+}
