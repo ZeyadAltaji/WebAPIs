0e9db2d445a8d8da2f5e69005ea2c4cdc21b89f3
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:14:36 +0300

    coding class UOW

--- ECommerce.DataAccessLayer/UnitOfWork.cs
+++ ECommerce.DataAccessLayer/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using ECommerce.Application.Abstractions;
 using ECommerce.Application.UnitOfWork;
 using ECommerce.DataAccessLayer.Repositories;
+using ECommerce.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,31 @@ namespace ECommerce.DataAccessLayer
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly DBContext dc;
+        private readonly DBContext DC;
         public UnitOfWork(DBContext dc)
         {
-            this.dc = dc;     
+            DC = dc;     
         }
-        public IUserRepository UserRepository => new UserRepository(dc);
+        public IUserRepository UserRepository => new UserRepository(DC);
+
+        public IRepository<Brands> repositoryBrands => new BrandsRepository(DC);
+
+        public IRepository<Car> repositoryCar => new CarRepository(DC);
+
+        public IRepository<Category> repositoryCategory => new CategoryRepository(DC);
+
+        public IRepository<Coupon> repositoryCoupon => new CouponRepository(DC);
+
+        public IRepository<Order> repositoryOrder => new OrderRepository(DC);
+
+        public IRepository<Product> repositoryProduct => new ProductRepository(DC);
+
+        public IRepository<Slider> repositorySlider => new SliderRepository(DC);
+
+        public IRepository<Special> repositorySpecial => new SpecialRepository(DC);
+
+        public IRepository<Sub_Slider> repositorySub_Slider => new Sub_SliderRepository(DC);
+
+        public IRepository<Vehicles> repositoryVehicles => new VehiclesRepository(DC);
     }
 }
