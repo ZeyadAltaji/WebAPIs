0b78e337444eedd0b20f4fa156454e91a43e0de0
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:18:58 +0300

    create class Sub_Slider Dtos with code

---
+++ ECommerce.Application/DTOs/Sub_SliderDTOs.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class Sub_SliderDTOs
+    {
+        public int Id { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Button { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+    }
+}
