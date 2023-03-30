eb1eec37ffb9af22d0a5314a30e283641c300203
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:01:45 +0300

    coding DTOs Vehicles

---
+++ ECommerce.Application/DTOs/VehiclesDTOs.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class VehiclesDTOs
+    {
+        public int Id { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+    }
+}
