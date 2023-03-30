6147174879b2a6d93432348ed4749de3bf2fe308
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-31 01:30:32 +0300

    create class Brands Dtos with code

---
+++ ECommerce.Application/DTOs/BrandsDTOs.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class BrandsDTOs : BaseClass
+    {
+        public string Image { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+    }
+}
