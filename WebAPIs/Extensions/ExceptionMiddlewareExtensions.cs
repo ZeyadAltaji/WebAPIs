5b46c80d880626d373596fb95ac8cfb2fe8bf941
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-05 02:53:42 +0300

    add Exception Middleware

---
+++ WebAPIs/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace WebAPIs.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+            }
+            else
+            {
+                app.UseExceptionHandler(
+                    option => option.Run(
+                        async context =>
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            var exception = context.Features.Get<IExceptionHandlerFeature>();
+                            if (exception != null)
+                            {
+                                await context.Response.WriteAsync(exception.Error.Message);
+                            }
+                        }));
+            }
+        }
+
+    }
+}
