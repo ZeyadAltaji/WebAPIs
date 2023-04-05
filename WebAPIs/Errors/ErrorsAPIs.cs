3a9edc757779eb24361ac21a79f0516fc6f9992a
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-05 03:15:01 +0300

    convert errors into JSON Text

---
+++ WebAPIs/Errors/ErrorsAPIs.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace WebAPIs.Errors
+{
+    public class ErrorsAPIs
+    {
+        public ErrorsAPIs(int Error_Code, string Error_Messages, string Errors_Details = null)
+        {
+            this.Errors_Details = Errors_Details;
+            this.Error_Code = Error_Code;
+            this.Error_Messages = Error_Messages;
+
+        }
+        public int Error_Code { get; set; }
+        public string Error_Messages { get; set; }
+        public string Errors_Details { get; set; }
+        public override string ToString()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return JsonSerializer.Serialize(this, options);
+        }
+    }
+}
