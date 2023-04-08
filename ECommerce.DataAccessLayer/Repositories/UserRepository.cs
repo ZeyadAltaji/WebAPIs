18a3b9de72a2782d2e129f04e862f3683139b2b1
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-09 01:02:00 +0300

    coding to Business Account

--- ECommerce.DataAccessLayer/Repositories/UserRepository.cs
+++ ECommerce.DataAccessLayer/Repositories/UserRepository.cs
@@ -32,23 +32,7 @@ namespace ECommerce.DataAccessLayer.Repositories
 
         }
 
-        public async Task<User> FindByEmailAsync(string Email)
-        {
-            var user = await DC.Users.FirstOrDefaultAsync(u => u.Email == Email);
-            if (user == null)
-            {
-                Console.WriteLine("User not found.");
-                return null; // or throw an exception if appropriate
-            }
-
-             DC.Users.Update(user);
-            await DC.SaveChangesAsync();
-
-            return user;
-
-        }
-
-        public void Register(string UserName, string password, string ComfirmPassword)
+        public void BusinessAccountRegister(string UserName, string Email, string password, string ComfirmPassword, int Role)
         {
             byte[] passwordHash, passwordKey;
 
@@ -66,18 +50,68 @@ namespace ECommerce.DataAccessLayer.Repositories
             {
                 User user = new User();
                 user.UserName = UserName;
+                user.Email = Email;
                 user.Password = passwordHash;
                 user.ComfirmPassword = passwordHash;
                 user.PasswordKey = passwordKey;
                 user.CreateDate = DateTime.Now;
+                user.Role = 2;
+
+                DC.Users.Add(user);
+            }
+        }
+
+        public async Task<User> FindByEmailAsync(string Email)
+        {
+            var user = await DC.Users.FirstOrDefaultAsync(u => u.Email == Email);
+            if (user == null)
+            {
+                Console.WriteLine("User not found.");
+                return null; // or throw an exception if appropriate
+            }
+
+             DC.Users.Update(user);
+            await DC.SaveChangesAsync();
+
+            return user;
+
+        }
+
+        public void Register(string UserName, string Frist_Name, string Last_Name, string Email, string password, string ComfirmPassword, int Role)
+        {
+            byte[] passwordHash, passwordKey;
+
+            using (var hmac = new HMACSHA512())
+            {
+                passwordKey = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            }
+            if (password != ComfirmPassword)
+            {
+                Console.WriteLine("Password In Valid ConfirmPassword ");
+            }
+            else
+            {
+                User user = new User();
+                user.UserName = UserName;
+                user.Frist_Name = Frist_Name;
+                user.Last_Name = Last_Name;
+                user.Email= Email;
+                user.Password = passwordHash;
+                user.ComfirmPassword = passwordHash;
+                user.PasswordKey = passwordKey;
+                user.CreateDate = DateTime.Now;
+                user.Role = 3;
                 
                 DC.Users.Add(user);
             }
         }
 
-        public async Task<bool> UserAlreadyExists(string UserName)
+        public async Task<bool> UserAlreadyExists(string UserName )
         {
-            return await DC.Users.AnyAsync(x=>x.UserName == UserName);
+            return await DC.Users.AnyAsync(x => x.UserName == UserName);
+
         }
 
         //Passwordalgorithm
