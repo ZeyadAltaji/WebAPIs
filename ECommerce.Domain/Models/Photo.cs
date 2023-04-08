6da8692097bbfcc0ced5f963ac2a72193c255294
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-09 00:48:25 +0300

    add table photo

---
+++ ECommerce.Domain/Models/Photo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Models
+{
+    public class Photo :BaseEntity
+    {
+        [Required]
+        public string publicID { get; set; }
+        [Required]
+        public string main_ImageUrl { get; set; }
+        public string sub_Image1Url { get; set; }
+        public string sub_Image2Url { get; set; }
+
+
+        public bool main_Image { get; set; }
+        public bool sub_Image1 { get; set; }
+        public bool sub_Image2 { get; set; }
+        public Product? Product { get; set; }
+    }
+}
