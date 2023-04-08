281afda765964e59a322d528d0ecb8f2135e3e10
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-09 01:00:38 +0300

    edit by Register and create methods Business Account

--- ECommerce.Application/Abstractions/IUserRepository.cs
+++ ECommerce.Application/Abstractions/IUserRepository.cs
@@ -10,8 +10,8 @@ namespace ECommerce.Application.Abstractions
     public interface IUserRepository
     {
         Task<User> Authenticate(string UserName, string password);
-        void Register(string UserName, string password,string ComfirmPassword);
-
+        void Register(string UserName, string Frist_Name, string Last_Name, string Email, string password,string ComfirmPassword, int Role);
+        void BusinessAccountRegister(string UserName, string Email, string password, string ComfirmPassword, int Role);
         Task<bool> UserAlreadyExists(string UserName);
         Task<User> FindByEmailAsync(string Email);
     }
