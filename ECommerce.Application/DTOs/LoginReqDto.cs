f7e06e418982f6dab7057ae06dd7d07032c09b4b
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-09 01:01:26 +0300

    change some in Login ReqDtos with comfoim with RestAPis

--- ECommerce.Application/DTOs/LoginReqDto.cs
+++ ECommerce.Application/DTOs/LoginReqDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace ECommerce.Application.DTOs
 {
     public class LoginReqDto
     {
+        public string Frist_Name { get; set; } = string.Empty;
+        public string Last_Name { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string ComfirmPassword { get; set; } = string.Empty;
+        public int Role { get; set; }
 
     }
 }
