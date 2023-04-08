87ca4b4f290c8e9688c35948edf05cb071407b97
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-09 00:44:42 +0300

    Create Interface Photo

---
+++ ECommerce.Application/Abstractions/IPhotoService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace ECommerce.Application.Abstractions
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> UploadPhotoAsync(IFormFile photo);
+      
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
