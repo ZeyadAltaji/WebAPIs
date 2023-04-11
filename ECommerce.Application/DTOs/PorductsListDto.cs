89e00cb350f6170df7a487b211e969fb581540f4
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-11 23:43:30 +0300

    add list Dtos

---
+++ ECommerce.Application/DTOs/PorductsListDto.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class PorductsListDto:BaseClass
+    {
+
+        public string Title { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public double Offers { get; set; }
+        public double NewPrice { get; set; }
+        public int Quantity { get; set; }
+ 
+        public int Customer_Id { get; set; }
+        public int Admin_Id { get; set; }
+        public User? User { get; set; }
+        public string mainImage { get; set; } = string.Empty;
+        public string subImage1 { get; set; } = string.Empty;
+        public string subImage2 { get; set; } = string.Empty;
+
+    }
+}
