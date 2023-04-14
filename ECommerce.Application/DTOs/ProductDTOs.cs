8601ab7c5678d095d97a8cd66105951be247fa2b
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-14 03:24:01 +0300

    edit some DTO Products

--- ECommerce.Application/DTOs/ProductDTOs.cs
+++ ECommerce.Application/DTOs/ProductDTOs.cs
@@ -9,6 +9,8 @@ namespace ECommerce.Application.DTOs
 {
     public class ProductDTOs:BaseClass
     {
+         public string Serial_Id { get; set; }
+
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string main_Image { get; set; } = string.Empty;
@@ -20,12 +22,9 @@ namespace ECommerce.Application.DTOs
         public int Quantity { get; set; }
         public string Link { get; set; } = string.Empty;
         public string Button { get; set; } = string.Empty;
-        public int Id_Brands { get; set; }
-        public Brands? Brands { get; set; }
-        public int Id_Car { get; set; }
-        public Car? Car { get; set; }
-        public int Category_Id { get; set; }
-        public Category? Category { get; set; }
+        public int? Brands_Id { get; set; }
+        public int? Car_Id { get; set; }
+        public int? Category_Id { get; set; }
         public int Customer_Id { get; set; }
         public int Admin_Id { get; set; }
         public User? User { get; set; }
