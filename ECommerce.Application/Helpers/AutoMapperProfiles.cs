21d1c491126ed2e4751761bd8b85215141d836bd
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-14 03:24:28 +0300

    tesing in migration

--- ECommerce.Application/Helpers/AutoMapperProfiles.cs
+++ ECommerce.Application/Helpers/AutoMapperProfiles.cs
@@ -15,8 +15,7 @@ namespace ECommerce.Application.Helpers
         {
             CreateMap<User, UserDTOs>().ReverseMap();
             CreateMap<Category, CategoryDTOs>().ReverseMap();
-            CreateMap<Product, ProductDTOs>().ReverseMap();
-            CreateMap<Brands,BrandsDTOs>().ReverseMap();
+             CreateMap<Brands,BrandsDTOs>().ReverseMap();
             CreateMap<Car, CarDTOs>().ReverseMap();
             CreateMap<Coupon, CouponDTOs>().ReverseMap();
             CreateMap<Order, OrderDTOs>().ReverseMap();
@@ -31,6 +30,15 @@ namespace ECommerce.Application.Helpers
                 .ForMember(dest => dest.mainImage, opt => opt.MapFrom(src => src.Image.FirstOrDefault(p => p.main_Image).main_ImageUrl))
                 .ForMember(dest => dest.subImage1, opt => opt.MapFrom(src => src.Image.FirstOrDefault(p => p.sub_Image1).sub_Image1Url))
                 .ForMember(dest => dest.subImage2, opt => opt.MapFrom(src => src.Image.FirstOrDefault(p => p.sub_Image2).sub_Image2Url));
+            CreateMap<Product, ProductDTOs>()
+            .ForMember(dest => dest.Brands_Id, opt => opt.MapFrom(src => src.BrandsId))
+            .ForMember(dest => dest.Car_Id, opt => opt.MapFrom(src => src.CarId))
+            .ForMember(dest => dest.Category_Id, opt => opt.MapFrom(src => src.CategoryId));
+
+            CreateMap<ProductDTOs, Product>()
+                .ForMember(dest => dest.BrandsId, opt => opt.MapFrom(src => src.Brands_Id))
+                .ForMember(dest => dest.CarId, opt => opt.MapFrom(src => src.Car_Id))
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Category_Id));
         }
     }
 }
