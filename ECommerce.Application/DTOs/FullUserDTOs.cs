45d5b90d14a31c5b4cf7ef311987d886d636e16b
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-15 06:48:15 +0300

    Create DTOs for user

---
+++ ECommerce.Application/DTOs/FullUserDTOs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class FullUserDTOs:BaseClass
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Frist_Name { get; set; } = string.Empty;
+        public string Last_Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public byte[] Password { get; set; }
+        public byte[] ComfirmPassword { get; set; }
+        public byte[] PasswordKey { get; set; }
+        public string Phone1 { get; set; } = string.Empty;
+        public string Phone2 { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int Role { get; set; }
+        public string Image { get; set; } = string.Empty;
+
+    }
+}
