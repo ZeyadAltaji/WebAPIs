ef97678c1b6e97f42f1629907db4b181a5434de5
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-15 22:46:26 +0300

    add user PhotoDtos

---
+++ ECommerce.Application/DTOs/UserPhotoDtos.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.DTOs
+{
+    public class UserPhotoDtos:BaseClass
+    {
+         public int user_Id { get; set; }
+        public string Image_userUrl { get; set; }
+
+        public string Public_id { get; set; }
+        public IFormFile file { get; set; }
+
+    }
+}
