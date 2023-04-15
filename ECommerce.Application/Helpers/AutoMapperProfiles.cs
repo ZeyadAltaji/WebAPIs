dcf93de38e3d5191036c0237fadc1bc2c35f1f62
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-15 22:46:37 +0300

    create mapper

--- ECommerce.Application/Helpers/AutoMapperProfiles.cs
+++ ECommerce.Application/Helpers/AutoMapperProfiles.cs
@@ -9,21 +9,21 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Application.Helpers
 {
-    public class AutoMapperProfiles:Profile
+    public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles()
         {
             CreateMap<User, UserDTOs>().ReverseMap();
-            CreateMap<User,FullUserDTOs>().ReverseMap();
+            CreateMap<User, FullUserDTOs>().ReverseMap();
             CreateMap<Category, CategoryDTOs>().ReverseMap();
-             CreateMap<Brands,BrandsDTOs>().ReverseMap();
+            CreateMap<Brands, BrandsDTOs>().ReverseMap();
             CreateMap<Car, CarDTOs>().ReverseMap();
             CreateMap<Coupon, CouponDTOs>().ReverseMap();
             CreateMap<Order, OrderDTOs>().ReverseMap();
             CreateMap<Slider, SliderDTOs>().ReverseMap();
             CreateMap<Special, SpecialDTOs>().ReverseMap();
             CreateMap<Sub_Slider, Sub_SliderDTOs>().ReverseMap();
-            CreateMap<Vehicles,VehiclesDTOs>().ReverseMap();
+            CreateMap<Vehicles, VehiclesDTOs>().ReverseMap();
             CreateMap<Photo, PhotoDTOs>().ReverseMap();
 
 
@@ -42,6 +42,11 @@ namespace ECommerce.Application.Helpers
                 .ForMember(dest => dest.CarId, opt => opt.MapFrom(src => src.Car_Id))
                 .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Category_Id))
             ;
+
+
+            CreateMap<UserPhotoDtos, User>()
+            .ForMember(dest => dest.Image_userUrl, opt => opt.MapFrom(src => src.Image_userUrl))
+            .ForMember(dest => dest.Image_userUrl, opt => opt.MapFrom(src => src.Public_id));
         }
     }
 }
